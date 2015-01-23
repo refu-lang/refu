867dc83731fa6ceb7f1e673efb9e5f5bafdefd44
Lefteris Karapetsas <lefteris@refu.co>
2015-01-23 01:51:19 +0100

    Some initial experimention with the IR(intermediate representation)

--- include/ir/elements.h
+++ include/ir/elements.h
@@ -1,14 +1,91 @@
 #ifndef LFR_IR_ELEMENTS_H
 #define LFR_IR_ELEMENTS_H
 
-// TODO: Total work in progress for elements of the intermediate representation
-//       format. For now simply played with the new STRUCT macros here.
-
+#include <Data_Structures/darray.h>
 #include <Utils/struct_utils.h>
+#include <types/type_decls.h>
 
-struct rir_basic_block {
-    int a;
+struct type;
+struct RFstring;
+struct ast_expression;
+
+/**
+ * Representation of a type for the Refu IR
+ */
+struct rir_type {
+    //! TODO: This should be an enum with all elementary types + 1 for composite type
+    //! In the case of composite type just define it somewhere else so that backends
+    //! like LLVM can have it as a struct
+    enum elementary_type elementary;
+    //! Array of types that may constitute this type. e.g: i64, u32, string
+    struct {darray(struct rir_type*);} subtypes;
+    //! Whether the @c subtypes are connected as product types (so by ,) or
+    //! as sum types (so by | )  TODO: Think if we will use this
+    bool are_product_type;
+
+    /* -- control attributes -- */
+    //! To be added to parameter lists (?)
+    //! TODO: Think if we will use this
+    struct RFilist_node lh;
 };
+struct rir_type *rir_type_alloc(struct type *input);
+struct rir_type *rir_type_create(struct type *input);
+bool rir_type_init(struct rir_type *type, struct type *input);
+
+void rir_type_dealloc(struct rir_type *t);
+void rir_type_destroy(struct rir_type *t);
+void rir_type_deinit(struct rir_type *t);
+
+/**
+ * Representation of a function for the Refu IR
+ */
+struct rir_function {
+    struct rir_type *arg_type;
+    struct rir_type *ret_type;
+    struct rir_basic_block *entry;
+    struct RFstring name;
+};
+RF_STRUCT_COMMON_SIGS_NO_ALLOC(rir_function, struct type *fn_type,
+                               struct RFstring *name);
+
+
+struct rir_simple_branch {
+    struct rir_basic_block *dst;
+};
+
+struct rir_cond_branch {
+    struct rir_simple_branch true_br;
+    struct rir_simple_branch false_br;
+    struct ast_expression *cond;
+};
+
+/**
+ * Represents a branching point in the Refu IR.
+ *
+ * A branching can either be conditional or unconditional in which
+ * case only a single destination needs to be specified.
+ */
+struct rir_branch {
+    bool is_conditional;
+    union {
+        struct rir_cond_branch cond_branch;
+        struct rir_simple_branch simple_branch;
+    };
+};
+
+/**
+ * Represents a basic block in the Refu IR.
+ *
+ * A basic block can only have some uninterrupted expressions and statements
+ * and has only one single exit.
+ */
+struct rir_basic_block {
+    //! List of expressions (?)
+
+    //! exit branch
+    struct rir_branch exit;
+};
+
 RF_STRUCT_COMMON_SIGS_NO_ALLOC(rir_basic_block, int a);
 
 
--- src/ir/elements.c
+++ src/ir/elements.c
@@ -1,31 +1,151 @@
 #include <ir/elements.h>
+#include <types/type.h>
+#include <types/type_elementary.h>
+#include <types/type_function.h>
+#include <ast/type_decls.h>
+#include <String/rf_str_decl.h>
+#include <String/rf_str_core.h>
 
-/* RF_STRUCT_ALLOC_SIG(rir_basic_block) */
-/* { */
-/*     struct rir_basic_block *ret; */
-/*     RF_MALLOC(ret, sizeof(*ret), return NULL); */
-/*     return ret; */
-/* } */
+static struct rir_type i_elementary_types[] = {
+#define INIT_ELEMENTARY_TYPE_ARRAY_INDEX(i_type)                           \
+    [i_type] = {.elementary = i_type}
 
-RF_STRUCT_INIT_SIG(rir_basic_block, int a)
+    INIT_ELEMENTARY_TYPE_ARRAY_INDEX(ELEMENTARY_TYPE_INT),
+    INIT_ELEMENTARY_TYPE_ARRAY_INDEX(ELEMENTARY_TYPE_UINT),
+    INIT_ELEMENTARY_TYPE_ARRAY_INDEX(ELEMENTARY_TYPE_INT_8),
+    INIT_ELEMENTARY_TYPE_ARRAY_INDEX(ELEMENTARY_TYPE_UINT_8),
+    INIT_ELEMENTARY_TYPE_ARRAY_INDEX(ELEMENTARY_TYPE_INT_16),
+    INIT_ELEMENTARY_TYPE_ARRAY_INDEX(ELEMENTARY_TYPE_UINT_16),
+    INIT_ELEMENTARY_TYPE_ARRAY_INDEX(ELEMENTARY_TYPE_INT_32),
+    INIT_ELEMENTARY_TYPE_ARRAY_INDEX(ELEMENTARY_TYPE_UINT_32),
+    INIT_ELEMENTARY_TYPE_ARRAY_INDEX(ELEMENTARY_TYPE_INT_64),
+    INIT_ELEMENTARY_TYPE_ARRAY_INDEX(ELEMENTARY_TYPE_UINT_64),
+    INIT_ELEMENTARY_TYPE_ARRAY_INDEX(ELEMENTARY_TYPE_FLOAT_32),
+    INIT_ELEMENTARY_TYPE_ARRAY_INDEX(ELEMENTARY_TYPE_FLOAT_64),
+    INIT_ELEMENTARY_TYPE_ARRAY_INDEX(ELEMENTARY_TYPE_STRING),
+    INIT_ELEMENTARY_TYPE_ARRAY_INDEX(ELEMENTARY_TYPE_BOOL),
+    INIT_ELEMENTARY_TYPE_ARRAY_INDEX(ELEMENTARY_TYPE_NIL)
+#undef INIT_ELEMENTARY_TYPE_ARRAY_INDEX
+};
+
+
+bool rir_type_init(struct rir_type *type, struct type *input)
 {
-    this->a = a;
+    struct rir_type *left_type_ptr;
+    struct rir_type *right_type_ptr;
+    darray_init(type->subtypes);
+
+    switch(input->category) {
+    case TYPE_CATEGORY_ELEMENTARY:
+    case TYPE_CATEGORY_GENERIC:
+        break;
+
+    case TYPE_CATEGORY_OPERATOR:
+        // TODO: fix this
+        if (input->operator.type != TYPEOP_PRODUCT) {
+            RF_ASSERT(false, "Sum types not yet supported at this stage");
+        }
+
+        if (!(left_type_ptr = rir_type_create(input->operator.left))) {
+            return false;
+        }
+        darray_push(type->subtypes, left_type_ptr);
+
+        if (!(right_type_ptr = rir_type_create(input->operator.right))) {
+            rir_type_destroy(left_type_ptr);
+            return false;
+        }
+        darray_push(type->subtypes, right_type_ptr);
+        break;
+
+    case TYPE_CATEGORY_LEAF:
+        if (!rir_type_init(type, input->leaf.type)) {
+            return false;
+        }
+        break;
+    }
+
     return true;
 }
 
-RF_STRUCT_DEINIT_SIG(rir_basic_block)
+struct rir_type *rir_type_alloc(struct type *input)
 {
-    return;
+    if (input->category == TYPE_CATEGORY_ELEMENTARY) {
+        return &i_elementary_types[type_elementary(input)];
+    }
+
+    // else
+    struct rir_type *ret;
+    RF_MALLOC(ret, sizeof(*ret), return NULL);
+    return ret;
+
 }
 
-/* RF_STRUCT_DEALLOC_SIG(rir_basic_block) */
-/* { */
-/*     free(this); */
-/* } */
+struct rir_type *rir_type_create(struct type *input)
+{
+    struct rir_type *ret = rir_type_alloc(input);
+    if (!ret) {
+        RF_ERROR("Failed at rir_type allocation");
+        return NULL;
+    }
+    if (!rir_type_init(ret, input)) {
+        RF_ERROR("Failed at rir_type initialization");
+        rir_type_dealloc(ret);
+    }
 
-/* RF_STRUCT_COMMON_DEFS_WITH_ALLOC(rir_basic_block, int, a) */
-RF_STRUCT_COMMON_DEFS_NO_ALLOC(rir_basic_block, int, a)
+    return ret;
+}
 
+void rir_type_dealloc(struct rir_type *t)
+{
+    // TODO: If t is not elementary then free
+    (void)t;
+}
+void rir_type_destroy(struct rir_type *t)
+{
+    rir_type_deinit(t);
+    rir_type_dealloc(t);
+}
+void rir_type_deinit(struct rir_type *t)
+{
+    struct rir_type **subtype;
+    darray_foreach(subtype, t->subtypes) {
+        rir_type_destroy(*subtype);
+    }
 
+    darray_free(t->subtypes);
+}
 
+/* -- rir_function -- */
 
+RF_STRUCT_INIT_SIG(rir_function, struct type* fn_type, struct RFstring *name)
+{
+    if (!rf_string_copy_in(&this->name, name)) {
+        RF_ERROR("failed to iniailize rir_function name");
+        return false;
+    }
+
+    this->arg_type = rir_type_create(type_function_get_argtype(fn_type));
+    if (!this->arg_type) {
+        RF_ERROR("Failed to create rir_function argument type");
+        return false;
+    }
+
+    this->ret_type = rir_type_create(type_function_get_rettype(fn_type));
+    if (!this->ret_type) {
+        RF_ERROR("Failed to create rir_function return type");
+        return false;
+    }
+
+    return true;
+}
+
+RF_STRUCT_DEINIT_SIG(rir_function)
+{
+    rf_string_deinit(&this->name);
+    rir_type_destroy(this->arg_type);
+    rir_type_destroy(this->ret_type);
+}
+
+RF_STRUCT_COMMON_DEFS_NO_ALLOC(rir_function, struct type*, fn_type,
+                               struct RFstring*, name)
